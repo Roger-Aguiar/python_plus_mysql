e9a92177f424be94cbcbbac7a32083b30baf5dda
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-07-21 16:10:37 -0300

    My first commit: Database setting module was created.

---
+++ database_config.py
@@ -0,0 +1,21 @@
+# Name:         Roger Silva Santos Aguiar
+# Function:     Database settings.
+# Initial date: July 21, 2020
+# Last update:  July 21, 2020
+
+# Required module
+import mysql.connector
+
+
+class DatabaseConnection:
+    @staticmethod
+    def my_connection():
+        mydb = mysql.connector.connect\
+            (
+                host='localhost',
+                user='root',
+                password='983453069',
+                database='COMPANY'
+            )
+
+        return mydb

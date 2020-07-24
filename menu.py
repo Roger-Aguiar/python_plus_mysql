4e883706534f78aa7b7db26f5fe823d5ec7be3f6
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-07-24 16:28:46 -0300

    Method to access the Employee files were created.

--- menu.py
+++ menu.py
@@ -1,10 +1,10 @@
 # Name:         Roger Silva Santos Aguiar
 # Function:     This is the main method to access the other modules
 # Initial date: July 22, 2020
-# Last update:  July 22, 2020
+# Last update:  July 24, 2020
 
 # Required modules
-import employee
+from Employee import employee_input
 
 
 class Menu:
@@ -23,10 +23,32 @@ class Menu:
 
         print("\n******************************************************************************************************************************************************\n")
 
+    def employee_menu(self):
+        input_employee = employee_input.EmployeeData()
+        input_employee.options()
+
+        option = int(input("\nChoose an option: "))
+
+        while option != 5:
+            if option == 1:
+                input_employee.insert()
+            elif option == 2:
+                input_employee.update()
+            elif option == 3:
+                input_employee.delete()
+            elif option == 4:
+                input_employee.get_table()
+
+            input_employee.options()
+            option = int(input("\nChoose an option: "))
+
 
 if __name__ == '__main__':
     menu = Menu()
 
     menu.options()
-
-    option = int(input("Choose an option: "))
+    option_menu = int(input("Chose an option: "))
+    if option_menu == 1:
+        menu.employee_menu()
+    else:
+        print("End program.")
